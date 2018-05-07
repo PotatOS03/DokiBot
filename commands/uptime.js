50f6702a49d7f7b739846855d4edafe966601d45
PotatOS03 <37935334+PotatOS03@users.noreply.github.com>
2018-05-06 22:17:20 -0400

    Add files via upload

--- commands/uptime.js
+++ commands/uptime.js
@@ -8,8 +8,8 @@ module.exports.run = async (bot, message, args) => {
     if (uptime >= 86400000) uptimeMsg += `${Math.floor(uptime / 86400000)}d, `;
     if (uptime >= 3600000) uptimeMsg += `${Math.floor((uptime % 86400000) / 3600000)}h, `;
     if (uptime >= 60000) uptimeMsg += `${Math.floor((uptime % 3600000) / 60000)}m, `;
-    uptimeMsg += `${(uptime % 60000) / 1000}s.`;
-
+    uptimeMsg += `${(uptime % 60000) / 1000}s`;
+    
     message.channel.send(`BOT UPTIME: \`${uptimeMsg}\``);
 }
 
